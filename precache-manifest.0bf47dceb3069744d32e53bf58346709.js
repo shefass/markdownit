f71ab828374cdabd1bc9f11f3e45de7982765c1b
Shefass <shefass@gmail.com>
2019-01-20 23:41:53 +0200

    Updates

---
+++ precache-manifest.0bf47dceb3069744d32e53bf58346709.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9de464a632ebe877ed6",
+    "url": "/markdownit/static/js/runtime~main.f9de464a.js"
+  },
+  {
+    "revision": "d47550a0ad9b4581461e",
+    "url": "/markdownit/static/js/main.d47550a0.chunk.js"
+  },
+  {
+    "revision": "c74f5028d93b26d8ce49",
+    "url": "/markdownit/static/js/1.c74f5028.chunk.js"
+  },
+  {
+    "revision": "d47550a0ad9b4581461e",
+    "url": "/markdownit/static/css/main.300b366d.chunk.css"
+  },
+  {
+    "revision": "76984d0f89be067bff808f3afaf5fe81",
+    "url": "/markdownit/index.html"
+  }
+];
